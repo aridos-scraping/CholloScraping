6e0f46f03462d5d1b3c97d0f80c3754386edbfa6
Angel Marmol Fernandez <angelmarmolfernandez@gmail.com>
2018-12-26 12:47:15 +0100

    Feliz navidad PAM PAM PAM PAM, feliz navidad, PAM PAM PARAM. Feliz navidad PROSPERO ANYO Y FELICIDAAAAAAD

--- cholloscraping/products/models.py
+++ cholloscraping/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Product(models.Model):
+    sku = models.CharField(primary_key=True, max_length=100)
+    brand = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Price(models.Model):
+
+    originalPrice = models.IntegerField()
+    currentPrice = models.IntegerField()
+    
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
