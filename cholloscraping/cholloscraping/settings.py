1948945a3e4d1af0268db54dfbd7f87e0ab9b520
Luis Rus Pegalajar <lrp_ruspeg@hotmail.com>
2019-01-23 02:56:05 +0100

    PostgreSQL

--- cholloscraping/cholloscraping/settings.py
+++ cholloscraping/cholloscraping/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'cholloscraping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cholloscraping',
+        'USER': 'cholloscraping',
+        'PASSWORD': '1234abcd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
