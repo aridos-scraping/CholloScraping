8e0a679088d4dd51c560828171a1c3289142e517
Luis Rus Pegalajar <lrp_ruspeg@hotmail.com>
2019-01-23 03:21:41 +0100

    Added migration file

--- cholloscraping/cholloscraping/settings.py
+++ cholloscraping/cholloscraping/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'cholloscraping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cholloscraping',
-        'USER': 'cholloscraping',
-        'PASSWORD': '1234abcd',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
